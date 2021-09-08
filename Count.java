e4650ad21512febcb83657918ef7d0b9df042b91
DeckyNatania <90346284+DeckyNatania@users.noreply.github.com>
2021-09-09 02:05:38 +0800

    Update Count.java

--- Count.java
+++ Count.java
@@ -1,1 +1,13 @@
+public class Count {  
 
+
+  public static int addition (int i1, int i2) {
+    int s=i1+i2;  
+    return s; //returning the sum  
+  }  
+
+  public static void main (String[] args) {
+    int c = addition (3,2);
+    System.out.println (c); 
+    }
+}
